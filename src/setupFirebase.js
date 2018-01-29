020a2998d91a73c52a8bdfefc2f46e75e8dff704
Dominik Dutkiewicz <dominikdut@gmail.com>
2018-01-29 22:34:58 +0100

    Changed firebase config

--- src/setupFirebase.js
+++ src/setupFirebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyCDp0pKIXfnPMbMPtVvhx6LNsIUQxQKDuM",
-  authDomain: "frontmeni-app.firebaseapp.com",
-  databaseURL: "https://frontmeni-app.firebaseio.com",
-  projectId: "frontmeni-app",
-  storageBucket: "",
-  messagingSenderId: "995766035206"
+  apiKey: "AIzaSyB4SwVHVUN8cyfe7Tn0kGETvvojtbpsFoY",
+  authDomain: "calco-fit-app.firebaseapp.com",
+  databaseURL: "https://calco-fit-app.firebaseio.com",
+  projectId: "calco-fit-app",
+  storageBucket: "calco-fit-app.appspot.com",
+  messagingSenderId: "281142169831"
 };
 
 export default () => {
